c52558789e54b317d84fd8fe2cc1aa1de6187664
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2023-01-15 19:58:23 +0200

    Add files via upload

---
+++ Inheritance-Exercise6/projectShop/main.py
@@ -0,0 +1,14 @@
+from projectPizzaMarker.drink import Drink
+from projectPizzaMarker.food import Food
+from projectPizzaMarker.product_repository import ProductRepository
+
+
+food = Food("apple")
+drink = Drink("water")
+repo = ProductRepository()
+repo.add(food)
+repo.add(drink)
+print(repo.products)
+print(repo.find("water"))
+repo.find("apple").decrease(5)
+print(repo)
