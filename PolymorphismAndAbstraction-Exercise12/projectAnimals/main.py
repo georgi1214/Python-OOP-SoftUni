98a1960fe17027437284ff89bd09e79ec2591e37
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2023-02-15 17:21:43 +0200

    Add files via upload

---
+++ PolymorphismAndAbstraction-Exercise12/projectAnimals/main.py
@@ -0,0 +1,18 @@
+from projectAnimals.cat import Cat
+from projectAnimals.dog import Dog
+from projectAnimals.kitten import Kitten
+from projectAnimals.tomcat import Tomcat
+
+dog = Dog("Rocky", 3, "Male")
+print(dog.make_sound())
+print(dog)
+tomcat = Tomcat("Tom", 6)
+print(tomcat.make_sound())
+print(tomcat)
+
+kitten = Kitten("Kiki", 1)
+print(kitten.make_sound())
+print(kitten)
+cat = Cat("Johnny", 7, "Male")
+print(cat.make_sound())
+print(cat)
